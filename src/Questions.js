43f742b4595d1481f70619f63b6b707bcca63bdc
Archie Cayabyab <archiejosephcayabyab@gmail.com>
2025-04-27 20:15:29 +0800

    fix: Fixed the image path by moving the image to the public directory.

--- src/Questions.js
+++ src/Questions.js
@@ -1,70 +1,70 @@
 const BrainrotQuestions = [
     {
         id: 1,
-        image: '/src/assets/images/1.webp',
+        image: 'assets/images/1.webp',
         question: 'Who is this?',
         options: ['Trippi Troppi', 'Tralalero Tralala', 'Lirilì Larilà', 'Bobrito Bandito'],
         answer: 'Tralalero Tralala'
     },
     {
         id: 2,
-        image: '/src/assets/images/2.webp',
+        image: 'assets/images/2.webp',
         question: 'Which one is shown?',
         options: ['Trippi Troppi', 'Chimpanzini Bananini', 'Girafa Celestre', 'Bombardiro Crocodilo'],
         answer: 'Trippi Troppi'
     },
     {
         id: 3,
-        image: '/src/assets/images/3.webp',
+        image: 'assets/images/3.webp',
         question: 'Name this one!',
         options: ['Tralalero Tralala', 'Capuccino Assassino', 'Tung Tung Tung Sahur', 'Trulimero Trulicina'],
         answer: 'Tung Tung Tung Sahur'
     },
     {
         id: 4,
-        image: '/src/assets/images/4.webp',
+        image: 'assets/images/4.webp',
         question: 'Which character is this?',
         options: ['Bobrito Bandito', 'Bombombini Gusini', 'Frigo Camelo', 'Chimpanzini Bananini'],
         answer: 'Chimpanzini Bananini'
     },
     {
         id: 5,
-        image: '/src/assets/images/5.webp',
+        image: 'assets/images/5.webp',
         question: 'Who is this?',
         options: ['Bombombini Gusini', 'Bombardiro Crocodilo', 'Brr Brr Patapim', 'Bobrito Bandito'],
         answer: 'Bombardiro Crocodilo'
     },
     {
         id: 6,
-        image: '/src/assets/images/6.webp',
+        image: 'assets/images/6.webp',
         question: 'Which character is this?',
         options: ['Frigo Camelo', 'Pot Hotspot', 'Trulimero Trulicina', 'Ballerina Cappucina'],
         answer: 'Ballerina Cappucina'
     },
     {
         id: 7,
-        image: '/src/assets/images/7.webp',
+        image: 'assets/images/7.webp',
         question: 'Name this one!',
         options: ['Garamaraman dan Madudungdung', 'Bombombini Gusini', 'Brri Brri Bicus Dicus Bombicus', 'Blueberrinni Octopussini'],
         answer: 'Bombombini Gusini'
     },
     {
         id: 8,
-        image: '/src/assets/images/8.webp',
+        image: 'assets/images/8.webp',
         question: 'Which one is shown?',
         options: ['Girafa Celestre', 'Burbaloni Lulilolli', 'Lirilì Larilà', 'Abonono Schimpazinono'],
         answer: 'Lirilì Larilà'
     },
     {
         id: 9,
-        image: '/src/assets/images/9.webp',
+        image: 'assets/images/9.webp',
         question: 'Which character is this?',
         options: ['Capuccino Assassino', 'Espressona Signora', 'Chef Crabracadabra', 'Il Cacto Hipopotamo'],
         answer: 'Capuccino Assassino'
     },
     {
         id: 10,
-        image: '/src/assets/images/10.webp',
+        image: 'assets/images/10.webp',
         question: 'Who is this?',
         options: ['Tigrrullini Watermellini', 'Graipussi Medussi', 'Orangutini Ananasini', 'Fruli Frula'],
         answer: 'Orangutini Ananasini'
